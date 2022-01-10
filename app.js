06ae9d688eb78a2d781029b0500faeb57df781ee
eraykalkan <eraykalkan6@gmail.com>
2022-01-10 23:21:09 +0300

    kursları listeleme controller ve view oluşturuldu

--- app.js
+++ app.js
@@ -7,15 +7,10 @@ const courseRoute = require("./routes/courseRoute")
 const app = express();
 
 //Connect DB
-await mongoose
-  .connect("mongodb://localhost/eduproject-db", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
+mongoose
+  .connect('mongodb://localhost/eduproject-db')
   .then(() => {
-    console.log("db connect Success");
+    console.log('DB Connected Successfully');
   });
 
 const port = 3000;
@@ -25,6 +20,8 @@ app.set("view engine", "ejs");
 
 //middlewares
 app.use(express.static("public"));
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.use("/", pageRoute);
 app.use("/courses", courseRoute);
