7d264d32ebba812c468f0ee0da9b6c1062fb3574
eraykalkan <eraykalkan6@gmail.com>
2022-01-10 20:11:10 +0300

    template parçalama yapıldı

--- app.js
+++ app.js
@@ -4,8 +4,22 @@ const app = express();
 
 const port = 3000;
 
+//Template Engine
+app.set("view engine","ejs")
+
+//middlewares
+app.use(express.static('public'))
+
 app.get("/", (req, res) => {
-  res.status(200).send("index");
+  res.render("index", {
+    page_name: "index"
+  })
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    page_name: "about"
+  })
 });
 
 app.listen(port, () => {
