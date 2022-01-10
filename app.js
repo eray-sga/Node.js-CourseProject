6f11456108d21113cbc1edc851177ae9dd34967c
eraykalkan <eraykalkan6@gmail.com>
2022-01-10 21:53:48 +0300

    kurs modeli ve routing oluşturuldu

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
 //önce npmden indirdiğim paketleri dahil ediyorum.
-const express = require('express');
-const mongoose = require('mongoose')
-const pageRoute = require('./routes/pageRoute');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const pageRoute = require("./routes/pageRoute");
+const courseRoute = require("./routes/courseRoute")
 
 const app = express();
 
 //Connect DB
-await mongoose.connect('mongodb://localhost/eduproject-db', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-}).then(() => {
-    console.log('db connect Success');
-});
+await mongoose
+  .connect("mongodb://localhost/eduproject-db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("db connect Success");
+  });
 
 const port = 3000;
 
 //Template Engine
-app.set("view engine","ejs")
+app.set("view engine", "ejs");
 
 //middlewares
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 app.use("/", pageRoute);
-
+app.use("/courses", courseRoute);
 
 app.listen(port, () => {
   console.log("sunucu çalışıyor");
