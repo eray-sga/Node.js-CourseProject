375a7d756e080bf33c30119ccc0990b6846a72b3
eraykalkan <eraykalkan6@gmail.com>
2022-01-15 17:40:05 +0300

    kurs silme işlemi yapıldı

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 //önce npmden indirdiğim paketleri dahil ediyorum.
 const express = require("express");
 const mongoose = require("mongoose");
-const session = require('express-session')
-const MongoStore = require('connect-mongo');
-const flash = require('connect-flash');
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
+const methodOverride = require("method-override");
 const pageRoute = require("./routes/pageRoute");
-const courseRoute = require("./routes/courseRoute")
-const categoryRoute = require("./routes/categoryRoute")
-const userRoute = require("./routes/userRoute")
+const courseRoute = require("./routes/courseRoute");
+const categoryRoute = require("./routes/categoryRoute");
+const userRoute = require("./routes/userRoute");
 
 const app = express();
 
 //Connect DB
-mongoose
-  .connect('mongodb://localhost/eduproject-db')
-  .then(() => {
-    console.log('DB Connected Successfully');
-  });
+mongoose.connect("mongodb://localhost/eduproject-db").then(() => {
+  console.log("DB Connected Successfully");
+});
 
 const port = 3000;
 
@@ -24,31 +23,39 @@ const port = 3000;
 app.set("view engine", "ejs");
 
 //Global variable
-global.userIN = null
+global.userIN = null;
 
 //middlewares
 
 app.use(express.static("public"));
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.use(session({
-  secret: 'my_keyboard_cat',
-  resave: false,
-  saveUninitialized: true,
-  store: MongoStore.create({ mongoUrl: 'mongodb://localhost/eduproject-db' })
-}))
+app.use(
+  session({
+    secret: "my_keyboard_cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: "mongodb://localhost/eduproject-db" }),
+  })
+);
 app.use(flash());
-app.use((req,res,next) => {
-  res.locals.flashMessages = req.flash()
-  next()
-})
- 
+app.use((req, res, next) => {
+  res.locals.flashMessages = req.flash();
+  next();
+});
+
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
+  })
+);
+
 //session olup olmadığını tutuyor
-app.use('*', (req, res, next) => {
-  userIN = req.session.userID
-  next()
-})
+app.use("*", (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
 
 app.use("/", pageRoute);
 app.use("/courses", courseRoute);
